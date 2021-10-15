13812a94939bf3d79a44d48be8f665766d4d2c94
Meg Wu <73263355+megwu1129@users.noreply.github.com>
2021-10-15 12:24:20 -0500

    Update breakout.py

--- breakout.py
+++ breakout.py
@@ -1,11 +1,3 @@
-"""
-stanCode Breakout Project
-Adapted from Eric Roberts's Breakout by
-Sonja Johnson-Yu, Kylie Jue, Nick Bowman,
-and Jerry Liao
-
-YOUR DESCRIPTION HERE
-"""
 
 from campy.gui.events.timer import pause
 from breakoutgraphics import BreakoutGraphics
